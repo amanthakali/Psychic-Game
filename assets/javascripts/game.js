5d0763cee4b2e4d2413551163449075e1f454986
aman thakali <amanthakali@hotmail.com>
2019-02-03 18:39:10 -0500

    separate jacascript file created

--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -1,57 +1,45 @@
-
-        
- //  Need a var for random alphabet
     var win = 0;
     var loss = 0;
-    var guessLeft =9;
-    
+    var guessLeft = 9;
+    var guessedLetter = [];
+
     // computer guess letter
-    var randomNumber = Math.floor(Math.random()*26);
+     
+    var randomNumber = Math.floor(Math.random() * 26);
     var randomChar = String.fromCharCode(97 + randomNumber);
-    //console.log(randomChar);
-
-    
+    console.log('DEBUG random Word --- ' +randomChar);
+ 
+      // geting the user guess char 
     document.onkeypress = function(event) {
-    var userGuess= event.key;
-    
-        if(userGuess=== randomChar){
-            win++;
-        }
-        else{
-            guessLeft--;
-            loss++;
-        }
-        //  if (guessLeft === 0){
-        //     reset();
-         }
+    var userGuess = event.key;
+    console.log(userGuess);
+
         
-
-
-        document.getElementById('wins').innerHTML= "Wins: " + win;
-        document.getElementById('losses').innerHTML="Losses: "+ loss;
-        document.getElementById('guessLeft').innerHTML="Guess Left: "+ guessLeft;
-        document.getElementById('userGuess').innerHTML="Your Guesses so far "+ userGuess;
-    
-    
-    //  var for win (initialize to 0)
-    //  var for loss (initialize to 0)
-    // var numberOFGuess; (initialize to 9)
-    //  var for userguess
-    //  create if else statement
-    // if (userguess==random albhabet){
-        // win++;
-    // }
-
-    // else{
-        // loss++;
-        // numberOfGuess--;
-    // }
-    // if(numberOfGuess == 0){
-        // reset ()
-    // }
-    // Create reset function
-    // reset function(){
+        //main logic of the game
+        if (userGuess === randomChar) {
+          win++;
+          guessLeft = 9;
+          guessedLetter = [];
         
-    // }
-
-
+        //creating the new random number once the guess is correct
+        var randomNumber = Math.floor(Math.random() * 26);
+        randomChar = String.fromCharCode(97 + randomNumber);
+        console.log('DEBUG random Word --- ' +randomChar);
+        }
+        
+        if (userGuess != randomChar) {
+          guessLeft--;
+          guessedLetter.push(userGuess);
+        }
+        if (guessLeft === 0) {
+          guessLeft = 9;
+          loss++;
+          guessedLetter = [];
+        }
+        document.getElementById("wins").innerHTML = "Wins: " + win;
+        document.getElementById("losses").innerHTML = "Losses: " + loss;
+        document.getElementById("guessLeft").innerHTML =
+          "Guess Left: " + guessLeft;
+        document.getElementById("userGuess").innerHTML =
+          "Your Guesses so far: " + guessedLetter.join(", ");
+      };
